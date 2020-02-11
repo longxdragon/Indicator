0029894b094ebc39f8f09e9b7e94d80bebc755e6
longxdragon <longxdragon@163.com>
2020-02-11 17:28:59 +0800

    Add framework proj for iOS

--- Samples/iOS/INCompiler/INCompiler/INCompiler.h
+++ Samples/iOS/INCompiler/INCompiler/INCompiler.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double INCompilerVersionNumber;
 FOUNDATION_EXPORT const unsigned char INCompilerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <INCompiler/PublicHeader.h>
-#import <INCompiler/PublicHeader.h>
+#import <INCompiler/INOCCompiler.h>
 
